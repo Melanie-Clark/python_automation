a761f892e7a4ae75f11538dc0a1228ce38736773
Melanie Clark <melaniefclark@gmail.com>
2026-01-24 15:53:37 +0000

    pyinputplus demo and explanation added.

---
+++ practice_files/validation_error_handling/automate_error_handling_part1.py
@@ -0,0 +1,9 @@
+# codecademy file
+try:
+    number = int(input("Enter a number: "))
+    result = 10 / number
+    print("The result is:",result)
+except ValueError:
+    print("You must enter a valid integer.")
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
