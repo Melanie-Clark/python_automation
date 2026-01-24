610af843e52a95c9d374141b2ca73c30ac1c8488
Melanie Clark <melaniefclark@gmail.com>
2026-01-24 15:18:05 +0000

    regex file updated

--- practice_files/regex/extract_data_with_regex_starter_code.py
+++ practice_files/regex/extract_data_with_regex_starter_code.py
@@ -1,14 +1,11 @@
 import re
 
-# uncomment the following line of code and fill in 
-# phoneNumRegex =
+phoneNumRegex = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
 
 example = "The number is 123-456-7890."
 
-# uncomment the following line of code and fill in 
-# result =
+result = phoneNumRegex.search(example)
 
-# uncomment the following lines of code and fill in 
-# if result:
-#     print("Phone number found:", # FILL IN #)
-#     print("Area code:", # FILL IN #)
+if result:
+    print("Phone number found:", result.group() )
+    print("Area code:", result.group()[0:3])
