5dfb70ff60e2303d227895ea78f7e905903c5cb8
Melanie Clark <melaniefclark@gmail.com>
2026-01-25 14:06:58 +0000

    parse_api_responses updated with json load and upc array

--- practice_files/api_practice/parse_api_responses_starter_code.py
+++ practice_files/api_practice/parse_api_responses_starter_code.py
@@ -1,45 +1,37 @@
-# import necessary libraries
 import requests
 import json
 
-# EXAMPLE 1: lemonade with raspberry
-print("\nPRODUCT EXAMPLE 1\n")
-# define base URL
-base_url = "https://api.upcitemdb.com/prod/trial/lookup"
-# define parameters
-parameters = {"upc": "025000044908"}
-# make API request, passing in base URL and parameters
-response = requests.get(base_url, params=parameters)
-# parse the text from the API response using JSON schema
-# info = FILL IN
-# extract the first item from info using the index 0
-item = info["items"][0]
-# extract the product’s title by indexing item
-title = item["title"]
-# extract the product’s brand by indexing item
-brand = item["brand"]
-# print out title
-print("title:", title)
-# print out brand
-print("brand:", brand)
+def get_upc_info(upcs):
+  n = 1 
+  for upc in upcs:
+    print(f"\nPRODUCT EXAMPLE {n}\n")
 
-# EXAMPLE 2: ridged potato chips
-print("\nPRODUCT EXAMPLE 2\n")
-# define base URL
-base_url = "https://api.upcitemdb.com/prod/trial/lookup"
-# define parameters
-parameters = {"upc": "028400516686"}
-# make API request, passing in base URL and parameters
-response = requests.get(base_url, params=parameters)
-# parse the text from the API response using JSON schema
-# info = FILL IN
-# extract the first item from info using the index 0
-item = info["items"][0]
-# extract the product’s title by indexing item
-title = item["title"]
-# extract the product’s brand by indexing item
-brand = item["brand"]
-# print out title
-print("title:", title)
-# print out brand
-print("brand:", brand)
+    # define base URL
+    base_url = "https://api.upcitemdb.com/prod/trial/lookup"
+
+    # upc code
+    parameters = {"upc": upc}
+
+    # make API request, passing in base URL and parameters
+    response = requests.get(base_url, params=parameters)
+
+    # parse the text from the API response using JSON schema
+    info = json.loads(response.text)
+
+    # extract the first item from info using the index 0
+    item = info["items"][0]
+
+    title = item["title"]
+    brand = item["brand"]
+
+    print("title:", title)
+    print("brand:", brand)
+    
+    n += 1
+    
+  
+get_upc_info(['615872592913','028400516686', '025000044908'])
+
+
+
+
